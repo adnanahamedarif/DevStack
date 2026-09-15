ecad0fd00635578eeaf30caa7fc6e7a1d1fe977f
Adnan Ahamed Arif <adnanahamedarif@gmail.com>
2026-09-16 00:15:36 +0600

    Make Hero section responsive

--- Dev-Stack/src/Components/Hero.tsx
+++ Dev-Stack/src/Components/Hero.tsx
@@ -3,21 +3,22 @@ import Banner from '../assets/banner-stack.png'
 
 const Hero = () => {
     return (
-        <div className='flex grid-cols-2 items-center py-20 container mx-auto'>
+        
+        <div className='flex flex-col md:flex-row items-center py-10 md:py-20 container mx-auto px-4 gap-'>
           {/* left */}
-            <div className='max-w-2xl'>
-                <h2 className='text-6xl font-bold '>Build Your Ideal <br /><span className='bg-gradient-to-r from-orange-500 via-pink-500 to-violet-500 bg-clip-text text-transparent'>Development Stack</span></h2>
+            <div className='max-w-2xl text-center md:text-left'>
+                <h2 className='text-4xl md:text-6xl font-bold'>Build Your Ideal <br /><span className='bg-gradient-to-r from-orange-500 via-pink-500 to-violet-500 bg-clip-text text-transparent'>Development Stack</span></h2>
 
 
-            <p className='mt-7 text-xl'>Explore frontend, backend, database, and tooling options,
+            <p className='mt-5 md:mt-7 text-base md:text-xl'>Explore frontend, backend, database, and tooling options,
                 compare them side by side, and put together the stack that fits your
                 next project.</p>
 
 
-                <div className='mt-8 flex  items-center gap-4'>
+                <div className='mt-8 flex flex-col sm:flex-row items-center justify-center md:justify-start gap-4'>
 
-                    <button className="rounded-xl bg-linear-to-r from-orange-500 via-pink-500 to-violet-500 px-6 py-3 font-semibold text-white cursor-pointer">Explore Technologies</button>
-                    <button className='rounded-xl border border-slate-200 bg-white px-6 py-3 text-sm font-semibold text-slate-700 cursor-pointer '>Learn More</button>
+                    <button className=' flex-1 sm:flex-none rounded-xl bg-linear-to-r from-orange-500 via-pink-500 to-violet-500 px-6 py-3 font-semibold text-white cursor-pointer'>Explore Technologies</button>
+                    <button className=' flex-1 sm:flex-none rounded-xl border border-slate-200 bg-white px-6 py-3 text-sm font-semibold text-slate-700 cursor-pointer '>Learn More</button>
 
                 </div>
 
@@ -27,7 +28,7 @@ const Hero = () => {
 
             {/* Right */}
 
-          <div className="w-full flex justify-end">
+          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
               <img src={Banner} alt="" />
           </div>
 
