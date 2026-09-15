17b4b3d90a2c8be90d13449261505b41d298f4b4
Adnan Ahamed Arif <adnanahamedarif@gmail.com>
2026-09-15 23:08:05 +0600

    make Nacbar Sticky

--- Dev-Stack/src/Components/Nav.tsx
+++ Dev-Stack/src/Components/Nav.tsx
@@ -2,29 +2,25 @@ import Logo from '../assets/logo-text.png'
 
 
 
-
 const Nav = () => {
     return (
-       <nav className='flex justify-between container mx-auto m-2 sticky'>
-           <img src={Logo} alt="" />            
-                <ul className='flex gap-4 items-center'>
+        <nav className='fixed top-0 left-0 w-full z-50 bg-white'>
+            <div className='flex justify-between container mx-auto py-2 px-4 items-center'>
+                <img src={Logo} alt="Logo" />            
+                <ul className='flex gap-4 items-center cursor-pointer'>
                     <li>Home</li>
                     <li>Technologies</li>
                     <li>Projects</li>
                     <li>About</li>
                     <li>Contact</li>
                 </ul>
-            
 
-            <div className='flex gap-4'>
-                <button>Sign In</button>
-                <button className='bg-pink-600 rounded-3xl text-white px-4 py-2 '>Sign Up</button>
+                <div className='flex gap-4 items-center '>
+                    <button className='cursor-pointer'>Sign In</button>
+                    <button className='bg-pink-500 rounded-3xl text-white px-4 py-2 cursor-pointer'>Sign Up</button>
+                </div>
             </div>
-           
-       </nav>
-
-
-
+        </nav>
     );
 };
 
